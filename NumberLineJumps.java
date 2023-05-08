135be7ec6b16b0d9575d0f0f600710ea7d518624
Subham Bhadra <70264692+subham-04@users.noreply.github.com>
2023-05-08 21:36:39 +0530

    Add files via upload

---
+++ NumberLineJumps.java
@@ -0,0 +1,35 @@
+class Result {
+
+    /*
+     * Complete the 'kangaroo' function below.
+     *
+     * The function is expected to return a STRING.
+     * The function accepts following parameters:
+     *  1. INTEGER x1
+     *  2. INTEGER v1
+     *  3. INTEGER x2
+     *  4. INTEGER v2
+     */
+
+    public static String kangaroo(int x1, int v1, int x2, int v2) {
+    
+    // if one kangaroos speed and position is greater than the other then they will never meet each other
+    if((x1>x2 && v1>v2) || (x2>x1 && v2>v1)) return "NO";
+
+
+
+    while(x1<=x2){
+        
+        if(x1==x2) {
+            return "YES";
+        }
+        
+        x1 = v1+x1;
+        x2 = v2+x2;
+        
+        
+    }
+    return "NO";
+    }
+
+}
