d18dcca3916525b88b40d74ca8cff35c4b5f34fe
Subham Bhadra <70264692+subham-04@users.noreply.github.com>
2023-05-08 21:47:49 +0530

    Update NumberLineJumps.java

--- NumberLineJumps.java
+++ NumberLineJumps.java
@@ -16,10 +16,10 @@ class Result {
     // if one kangaroos speed and position is greater than the other then they will never meet each other
     if((x1>x2 && v1>v2) || (x2>x1 && v2>v1)) return "NO";
 
-
+ //So, we need to add an extra condition to check if the position of one kangaroo is less than the other and its speed is also less than or equal to the other kangaroo's speed, then return "NO".
 
     while(x1<=x2){
-        
+        // if their distance becomes equal then return yes
         if(x1==x2) {
             return "YES";
         }
