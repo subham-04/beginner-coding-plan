58f81a491784a7d172df4f4ef53f5f09b252c365
Subham Bhadra <70264692+subham-04@users.noreply.github.com>
2023-05-14 13:17:19 +0530

    Add files via upload

---
+++ CatandMouse.java
@@ -0,0 +1,18 @@
+public class Solution {
+
+    // Complete the catAndMouse function below.
+    static String catAndMouse(int x, int y, int z) {
+        
+        int diff_XZ = z-x;
+        diff_XZ=Math.abs(diff_XZ);
+        int diff_YZ = z-y;
+        diff_YZ=Math.abs(diff_YZ);
+        
+        if(diff_XZ<diff_YZ){
+            return "Cat A";
+        }else if(diff_YZ<diff_XZ){
+            return "Cat B";
+        }
+        
+        return "Mouse C";
+    }
