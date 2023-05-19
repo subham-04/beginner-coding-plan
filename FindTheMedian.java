3081e922684c8ab20856de85ed4d8b7ec4ab6523
Subham Bhadra <70264692+subham-04@users.noreply.github.com>
2023-05-19 17:08:03 +0530

    Add files via upload

---
+++ FindTheMedian.java
@@ -0,0 +1,18 @@
+
+class Result {
+
+    /*
+     * Complete the 'findMedian' function below.
+     *
+     * The function is expected to return an INTEGER.
+     * The function accepts INTEGER_ARRAY arr as parameter.
+     */
+
+    public static int findMedian(List<Integer> arr) {
+    // Write your code here
+        Collections.sort(arr);
+        
+        return arr.get(arr.size()/2);
+    }
+
+}
